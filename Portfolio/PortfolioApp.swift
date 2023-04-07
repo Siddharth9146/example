90f0441fe5db410291edbb4bcf6419dd9b1969e9
Siddharth Rajopadhye <rajopadhyesiddharth@gmail.com>
2023-04-08 01:39:11 +0530

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Siddharth Rajopadhye on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
